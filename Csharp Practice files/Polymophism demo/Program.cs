7d0e7ab141becec48f850ab7e16f75f56f722b54
siva prasad k <153076202+sivaprasadkvd@users.noreply.github.com>
2023-12-11 20:02:02 +0530

    Add files via upload

---
+++ Csharp Practice files/Polymophism demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// polymorphism is a mechanism of changing the behavior based on the inputs given
+
+namespace Polymophism_demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pte = new PartTimeEmployee();
+            pte.firstName = "siva";
+            pte.lastName = "prasad";
+    
+
+            pte.PrintFullName();
+
+
+        }
+    }
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+
+        public virtual void PrintFullName()
+        {
+            Console.WriteLine("Your full name is: " + firstName + " " + lastName);
+        }
+    }
+
+   
+    public class PartTimeEmployee : Employee
+    {
+        public override void PrintFullName()
+        {
+            Console.WriteLine("Your full name is: " + firstName + " " + lastName + " child flag");
+        }
+    }
+
+
+    
+}
